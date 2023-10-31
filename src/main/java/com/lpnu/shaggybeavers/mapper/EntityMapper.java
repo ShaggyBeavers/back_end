90fb910659f0e371ed2696acad04cde77d285aba
biliakandrii <spider11A@ukr.net>
2023-10-31 18:21:41 +0200

    Mapper example fixed

---
+++ src/main/java/com/lpnu/shaggybeavers/mapper/EntityMapper.java
@@ -0,0 +1,7 @@
+package com.lpnu.shaggybeavers.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface EntityMapper extends UserMapper {
+}
---
+++ src/main/java/com/lpnu/shaggybeavers/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.lpnu.shaggybeavers.mapper;
+
+import com.lpnu.shaggybeavers.dto.UserDTO;
+import com.lpnu.shaggybeavers.model.User;
+
+public interface UserMapper {
+
+    UserDTO toUserDTO(User user);
+}
