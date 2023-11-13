25f558e0aaef1958ea9cbcbfaef611a32a773404
biliakandrii <spider11A@ukr.net>
2023-11-13 21:27:14 +0200

    Role init added

--- src/main/java/com/lpnu/shaggybeavers/factory/RoleFactory.java
+++ src/main/java/com/lpnu/shaggybeavers/factory/RoleFactory.java
@@ -1,6 +1,7 @@
 package com.lpnu.shaggybeavers.factory;
 
 import com.lpnu.shaggybeavers.mapper.EntityMapper;
+import com.lpnu.shaggybeavers.model.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,4 +11,7 @@ public class RoleFactory {
 
     private final EntityMapper entityMapper;
 
+    public Role toRole(String name) {
+        return entityMapper.toRole(name);
+    }
 }
