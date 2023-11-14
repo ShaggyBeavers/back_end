52a692d39cec650b3e76c97fcda3db56da18df4b
AndriiOsidach <andriiosidach@icloud.com>
2023-11-14 12:30:01 +0200

    Remove redundant method in RelicService

--- src/main/java/com/lpnu/shaggybeavers/service/RelicService.java
+++ src/main/java/com/lpnu/shaggybeavers/service/RelicService.java
@@ -2,8 +2,5 @@ package com.lpnu.shaggybeavers.service;
 
 import com.lpnu.shaggybeavers.model.Relic;
 
-import java.net.URL;
-
 public interface RelicService extends CRUDService<Relic,Long> {
-    void setImageUrl(URL url, Long relicId);
 }
